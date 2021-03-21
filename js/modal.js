f6f177488372bc65916f8423a599f840f9f763f4
”Ivan <yana.zakharchenko10@gmail.com>
2021-03-21 18:12:18 +0200

    new

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[mobile-modal-open]'),
-    closeModalBtn: document.querySelector('[mobile-modal-close]'),
-    modal: document.querySelector('[mobile-modal]'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('mobile-modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
